452f49631652b313fc9dcda39be8be90c883deb4
Alejandro Hernandez <alejandrojaez@outlook.com>
2021-07-04 22:47:17 -0500

    added delete h265 files

---
+++ deleteh265files.py
@@ -0,0 +1,17 @@
+import glob
+import subprocess
+import json
+from pathlib import Path
+from pprint import pprint
+
+files = glob.glob('E:\Videos' + '/**/*.mp4', recursive=True)
+for file in files:
+    try:
+        info = subprocess.check_output(['ffprobe', '-show_format', '-show_streams', '-loglevel', 'quiet', '-print_format', 'json', file])
+        video = json.loads(info)
+        if video["streams"][0]["codec_name"] == 'hevc':
+            #print(video["streams"][0]["codec_name"])
+            print("deleting: ",file)
+            hevc_file = Path(file).unlink()
+    except:
+        pass
