758b96ea28ca52e514ed0fd39d2ad1b387805723
Alejandro Hernandez <alejandrojaez@outlook.com>
2021-07-06 20:01:07 -0500

    upd

--- imei_list.py
+++ imei_list.py
@@ -1,10 +1,16 @@
 import re
-imeis_string = '464564646465464, 454546465464646,  454646564646454'
+imeis_string = '355316082611279, 351756051523999,  490154203237519'
 
+def valid_imei(x): return True if sum(sum(int(i) for i in str(int(x)*2)) if idx %2 == 1 else int(x) for idx, x in enumerate(str(x))) % 10 == 0 else False
 
 def imei_extractor(v):
     if bool(re.match(r'\d{15}([,\s]\s*\d{15})*$', v)):
-        return re.split(r'\W+', v)
+        imeis = re.split(r'\W+', v)
+        for imei in imeis:
+            if not valid_imei(int(imei.strip())):
+                raise ValueError(imei,"no es un imei valido")
+            else:
+                return imeis
     else:
         raise ValueError
 
--- imei_validate.py
+++ imei_validate.py
@@ -1,7 +1,7 @@
 
 imei1 = 355316082611279
 imei2 = 490154203237518
-imei3 = 490154203237517
+imei3 = 351756051523999
 
 
 def valid_imei(x): return True if sum(sum(int(i) for i in str(int(x)*2)) if idx %2 == 1 else int(x) for idx, x in enumerate(str(x))) % 10 == 0 else False
@@ -12,4 +12,5 @@ def valid_lenght(value: str):
 
 
 print(imei1, valid_imei(imei1))
-print(imei1, valid_lenght(imei1))
+print(imei2, valid_imei(imei2))
+print(imei3, valid_imei(imei3))
