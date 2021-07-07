3a0d8f280e214180aaa4989f274bc33f94a47d7d
Alejandro Hernandez <alejandrojaez@outlook.com>
2021-07-07 02:50:41 -0500

    imei_list works

--- imei_list.py
+++ imei_list.py
@@ -1,5 +1,5 @@
 import re
-imeis_string = '355316082611279, 351756051523999,  490154203237519'
+imeis_string = '355316082611279, 355316082611279,  490154203237519'
 
 def valid_imei(x): return True if sum(sum(int(i) for i in str(int(x)*2)) if idx %2 == 1 else int(x) for idx, x in enumerate(str(x))) % 10 == 0 else False
 
@@ -7,13 +7,11 @@ def imei_extractor(v):
     if bool(re.match(r'\d{15}([,\s]\s*\d{15})*$', v)):
         imeis = re.split(r'\W+', v)
         for imei in imeis:
-            if not valid_imei(int(imei.strip())):
+            print(valid_imei(int(imei)))
+            if not valid_imei(int(imei)):
                 raise ValueError(imei,"no es un imei valido")
-            else:
-                return imeis
+        return imeis
     else:
         raise ValueError
-
-#regex = /^\d{15}([,\s]\s*\d{15})*$/gm
 
 print(imei_extractor(imeis_string))
